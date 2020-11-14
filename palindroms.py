69fae8950e67ad5a8101798f0d4e42544e3e81a6
Konrad Gawron <konrad.gawron@poczta.fm>
2020-11-14 14:29:05 +0100

    Add bool True/False

--- palindroms.py
+++ palindroms.py
@@ -2,9 +2,11 @@ def palindrome():
     """
           Print result if word is palindrome or not
           Set conditional to compare string/rev string
-          Add bool True
+          Add bool True/False
           Add input
           Use another condition to check true or false
     """
     rev_string = string[::-1]
     if string != rev_string:
+        return False
+    return True
