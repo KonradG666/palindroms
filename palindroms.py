2c73f24ae0bb5e4f3aa2d5a833d2e00e2f24e237
Konrad Gawron <konrad.gawron@poczta.fm>
2020-11-14 14:27:16 +0100

    Add function and checking condition

---
+++ palindroms.py
@@ -0,0 +1,10 @@
+def palindrome():
+    """
+          Print result if word is palindrome or not
+          Set conditional to compare string/rev string
+          Add bool True
+          Add input
+          Use another condition to check true or false
+    """
+    rev_string = string[::-1]
+    if string != rev_string:
