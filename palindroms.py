d1260e69a203c29973c9cc2efaa1bd8ae4464629
Konrad Gawron <konrad.gawron@poczta.fm>
2020-11-14 14:47:33 +0100

    Add branch logging

--- palindroms.py
+++ palindroms.py
@@ -18,3 +18,29 @@ if __name__ == "__main__":
         print("The string is a palindrome.")
     else:
         print("The string is not a palindrome.")
+
+import sys
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
+logging.debug("debug")
+
+def palindrome():
+    """
+          Print result if word is palindrome or not
+          Set conditional to compare string/rev string
+          Add bool True/False
+          Use another condition to check true or false
+    """
+    rev_string = string[::-1]
+    if string != rev_string:
+        return False
+    return True
+
+if __name__ == "__main__":
+    logging.debug("The program was called with this parameters %s" % sys.argv[1])
+    string = sys.argv[1]
+    check = palindrome()
+    if check:
+        logging.debug("The string is a palindrome.")
+    else:
+        logging.debug("The string is not a palindrome."
